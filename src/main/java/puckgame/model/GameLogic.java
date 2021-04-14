59506dd673069c8a55da21b04c521ce7914db027
Máté Puskás <puskasmate2000@gmail.com>
2021-04-14 18:57:21 +0200

    Created the isValidMove method to check if the wanted move is valid.

--- src/main/java/puckgame/model/GameLogic.java
+++ src/main/java/puckgame/model/GameLogic.java
@@ -21,4 +21,81 @@ public class GameLogic {
         this.grid = grid;
     }
 
+    public boolean isValidMove(Player player, int row, int col, int direction) {
+
+        boolean isValid = false;
+
+        if (player.getPlayerId() == 1 && grid[row][col] == 2 || !(player.getPlayerId() == 1) && grid[row][col] == 1) {
+            isValid = false;
+        }
+
+        else {
+            if (player.getPlayerId() == 1) {
+                switch (direction) {
+                    case RIGHT: {
+                        if (col != grid.length-1 && grid[row][col+1] == 2) {
+                            isValid = true;
+                        }
+                        break;
+                    }
+                    case LEFT: {
+                        if (col != 0 && grid[row][col-1] == 2) {
+                            isValid = true;
+                        }
+                        break;
+                    }
+                    case UP: {
+                        if (row != 0 && grid[row-1][col] == 2) {
+                            isValid = true;
+                        }
+                        break;
+                    }
+                    case DOWN: {
+                        if (row != grid.length-1 && grid[row+1][col] == 2) {
+                            isValid = true;
+                        }
+                        break;
+                    }
+                    default:
+                        isValid = false;
+                }
+            }
+            else {
+                if (player.getPlayerId() == 2) {
+                    switch (direction) {
+                        case RIGHT: {
+                            if (col != grid.length-1 && grid[row][col+1] == 0) {
+                                isValid = true;
+                            }
+                            break;
+                        }
+                        case LEFT: {
+                            if (col != 0 && grid[row][col-1] == 0) {
+                                isValid = true;
+                            }
+                            break;
+                        }
+                        case UP: {
+                            if (row != 0 && grid[row-1][col] == 0) {
+                                isValid = true;
+                            }
+                            break;
+                        }
+                        case DOWN: {
+                            if (row != grid.length-1 && grid[row+1][col] == 0) {
+                                isValid = true;
+                            }
+                            break;
+                        }
+                        default:
+                            isValid = false;
+                    }
+                }
+            }
+
+        }
+        return isValid;
+    }
+
+
 }
