b733748f1e2713959a617fd4dc06cab159ffdc9c
Máté Puskás <puskasmate2000@gmail.com>
2021-04-14 15:56:20 +0200

    Added the main class for the project with a basic stage.

---
+++ src/main/java/puckgame/javafx/Main.java
@@ -0,0 +1,12 @@
+package puckgame.javafx;
+
+import javafx.application.Application;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Application.launch(PuckGameApp.class);
+    }
+
+}
---
+++ src/main/java/puckgame/javafx/PuckGameApp.java
@@ -0,0 +1,19 @@
+package puckgame.javafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PuckGameApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/game.fxml"));
+        primaryStage.setTitle("Puck Game");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+}
