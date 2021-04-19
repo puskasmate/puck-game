de92d5e0579060c7a92b2fde1113f776472f4c35
Máté Puskás <puskasmate2000@gmail.com>
2021-04-19 17:52:20 +0200

    Created test class and tested the isValidMove method at the starting state.

---
+++ src/test/java/puckgame/GameLogicTest.java
@@ -0,0 +1,54 @@
+package puckgame;
+
+import org.junit.jupiter.api.Test;
+import puckgame.model.GameLogic;
+import puckgame.model.Player;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameLogicTest {
+
+    GameLogic gameLogic;
+    Player player1;
+    Player player2;
+
+    @Test
+    void testIsValidMoveAtStartingState() {
+        gameLogic = new GameLogic();
+        player1 = new Player("p1", 1, 0);
+        player2 = new Player("p2", 2, 0);
+
+        //middle blue
+        assertTrue(gameLogic.isValidMove(player1, 2, 2, 0));
+        assertTrue(gameLogic.isValidMove(player1, 2, 2, 1));
+        assertTrue(gameLogic.isValidMove(player1, 2, 2, 2));
+        assertTrue(gameLogic.isValidMove(player1, 2, 2, 3));
+        assertFalse(gameLogic.isValidMove(player1, 2, 2, 4));
+        assertFalse(gameLogic.isValidMove(player2, 2, 2, 0));
+
+        //top blue
+        assertFalse(gameLogic.isValidMove(player1, 0, 4, 0));
+        assertTrue(gameLogic.isValidMove(player1, 0, 4, 1));
+        assertFalse(gameLogic.isValidMove(player1, 0, 4, 2));
+        assertTrue(gameLogic.isValidMove(player1, 0, 4, 3));
+        assertFalse(gameLogic.isValidMove(player1, 0, 4, 4));
+        assertFalse(gameLogic.isValidMove(player2, 0, 4, 1));
+
+        //bottom blue
+        assertTrue(gameLogic.isValidMove(player1, 4, 0, 0));
+        assertFalse(gameLogic.isValidMove(player1, 4, 0, 1));
+        assertTrue(gameLogic.isValidMove(player1, 4, 0, 2));
+        assertFalse(gameLogic.isValidMove(player1, 4, 0, 3));
+        assertFalse(gameLogic.isValidMove(player1, 4, 0, 4));
+        assertFalse(gameLogic.isValidMove(player2, 4, 0, 0));
+
+        //random red puck
+        assertFalse(gameLogic.isValidMove(player2, 0, 0 ,0));
+        assertFalse(gameLogic.isValidMove(player2, 0, 0 ,1));
+        assertFalse(gameLogic.isValidMove(player2, 0, 0 ,2));
+        assertFalse(gameLogic.isValidMove(player2, 0, 0 ,3));
+        assertFalse(gameLogic.isValidMove(player2, 0, 0 ,7));
+        assertFalse(gameLogic.isValidMove(player1, 0, 0 ,0));
+    }
+
+}
