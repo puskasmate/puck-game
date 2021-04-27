c00593f8c64513f5a1a9e7b193ca96f1749d183c
Máté Puskás <puskasmate2000@gmail.com>
2021-04-27 11:47:12 +0200

    Added javadoc to GameResult.

--- src/main/java/puckgame/results/GameResult.java
+++ src/main/java/puckgame/results/GameResult.java
@@ -10,6 +10,9 @@ import javax.persistence.*;
 import java.time.Duration;
 import java.time.ZonedDateTime;
 
+/**
+ * Class representing the result of a game played by a specific player.
+ */
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -17,22 +20,40 @@ import java.time.ZonedDateTime;
 @Entity
 public class GameResult {
 
+    /**
+     * The id of the result in database.
+     */
     @Id
     @GeneratedValue
     private Long id;
 
+    /**
+     * The name of the player.
+     */
     @Column(nullable = false)
     private String name;
 
+    /**
+     * The color of the puck the player played with.
+     */
     @Column(nullable = false)
     private String color;
 
+    /**
+     * The duration of the game.
+     */
     @Column(nullable = false)
     private Duration duration;
 
+    /**
+     * The time when the player won the game.
+     */
     @Column(nullable = false)
     private ZonedDateTime created;
 
+    /**
+     * A method that creates the time when did the player win.
+     */
     @PrePersist
     protected void onPersist() {
         created = ZonedDateTime.now();
